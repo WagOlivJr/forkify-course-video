8315970356fe905d351c23dd18c0b6451859edf5
wagner-oliveira-ey <wagner.oliveira@br.ey.com>
2024-10-18 14:52:21 -0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -125,6 +125,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the Application');
+};
+
 const init = function () {
   recipeView.addHandlerRander(controlBookmarks);
   recipeView.addHandlerRander(controlRecipes);
@@ -133,6 +137,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerPaginationBtnsClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
